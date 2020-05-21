e19cc01cd09ba6324c062f1717a40e371c801c60
dmitrynjg <49000073+dmitrynjg@users.noreply.github.com>
2020-05-21 13:27:47 +0300

    Updates

---
+++ precache-manifest.680eab947902f68f2312ae757913d0af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5126c038b47938fc0881c903bc810ce",
+    "url": "/project-react-chart/index.html"
+  },
+  {
+    "revision": "ae546ec2461995b318c7",
+    "url": "/project-react-chart/static/css/main.a40ec5e3.chunk.css"
+  },
+  {
+    "revision": "5f79b05c18b7ba5fc2d2",
+    "url": "/project-react-chart/static/js/2.70cd4403.chunk.js"
+  },
+  {
+    "revision": "ae546ec2461995b318c7",
+    "url": "/project-react-chart/static/js/main.3452eacd.chunk.js"
+  },
+  {
+    "revision": "13dbd91641e1070a67e5",
+    "url": "/project-react-chart/static/js/runtime~main.266dc198.js"
+  }
+]);
